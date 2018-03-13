d94e8ed147412518b870333fbfda5be78cd48476
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2018-03-13 22:32:15 +0000

    Atempting to add cron

--- nginx_rpi/resources/duck.sh
+++ nginx_rpi/resources/duck.sh
@@ -1,3 +1,2 @@
 # HomeAssistant Domain DuckDns update
-echo url="https://www.duckdns.org/update?domains=__YOUR-DOMAIN__&token=__YOUR-TOKEN__&ip=" | curl -k -o ~/duckdns/duck.log -K -
-
+echo url="https://www.duckdns.org/update?domains=__DOMAIN__&token=__TOKEN__&ip=" | curl -k -o /duckdns/duck.log -K -
