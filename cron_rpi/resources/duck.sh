b2aa29a09f89539670595c2f6c45e5a410115ca5
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2018-06-01 22:25:54 +0000

    Using env to create cron docker container

--- cron_rpi/resources/duck.sh
+++ cron_rpi/resources/duck.sh
@@ -1,2 +1,2 @@
 # HomeAssistant Domain DuckDns update
-echo url="https://www.duckdns.org/update?domains=__DOMAIN__&token=__TOKEN__&ip=" | curl -k -o /duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=" | curl -k -o /duckdns/duck.log -K -
