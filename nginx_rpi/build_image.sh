41d4f9a27dde704a014670d745b7939442ee435a
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2018-06-01 22:38:16 +0000

    Swapping conf file on build

--- nginx_rpi/build_image.sh
+++ nginx_rpi/build_image.sh
@@ -1,2 +1,10 @@
+# Swap
+mv nginx_conf/homeassistant.conf nginx_conf/homeassistant.conf_build
+mv nginx_conf/secret nginx_conf/homeassistant.conf
+
 # Build the image from Dockerfile
 docker build --pull -t nginx-server-rpi:latest .
+
+# Cleanup
+mv nginx_conf/homeassistant.conf nginx_conf/secret
+mv nginx_conf/homeassistant.conf_build nginx_conf/homeassistant.conf
