1f2b636ededb9f3db10a28b09396e3489a15696f
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2018-06-03 11:29:55 +0200

    Naming

--- emoncms/cron/resources/duck.sh
+++ emoncms/cron/resources/duck.sh
@@ -1,2 +1,2 @@
-# HomeAssistant Domain DuckDns update
+# EmonCMS Domain DuckDns update
 echo url="https://www.duckdns.org/update?domains=__DUCK_DOMAIN__&token=__DUCK_TOKEN__&ip=" | curl -k -o /duckdns/duck.log -K -
